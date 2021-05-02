988e420fd5f1013684ab9b3e5fa581302f614934
scw3812 <dlffpraos@naver.com>
2021-05-03 02:35:25 +0900

    삽질

---
+++ react_navigation_test/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
