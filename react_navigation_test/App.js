a9951c9bc0fbfe4ea7ca3bc97734a482ce748382
scw3812 <dlffpraos@naver.com>
2021-05-18 15:21:40 +0900

    nesting navigators

--- react_navigation_test/App.js
+++ react_navigation_test/App.js
@@ -1,29 +1,57 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {Image} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
+import {View, TouchableOpacity, Text} from 'react-native';
+import {
+  NavigationContainer,
+  DrawerActions,
+  useNavigation,
+} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-// import {
-//   createDrawerNavigator,
-//   DrawerContentScrollView,
-//   DrawerItem,
-//   DrawerItemList,
-// } from '@react-navigation/drawer';
-// import HomeScreen from './src/home';
+import {
+  createDrawerNavigator,
+  // DrawerContentScrollView,
+  // DrawerItem,
+  // DrawerItemList,
+} from '@react-navigation/drawer';
+import HomeScreen from './src/home';
 // import UserScreen from './src/user';
 // import Logo from './src/logo';
 // import HomeDrawer from './src/home_drawer';
 // import UserDrawer from './src/user_drawer';
-// import MyDrawer from './src/my_drawer';
+import MyDrawer from './src/my_drawer';
 import TabHomeScreen from './src/home_tab';
 import TabUserScreen from './src/user_tab';
 import TabMessageScreen from './src/message_tab';
 import Ionicons from 'react-native-vector-icons/dist/Ionicons';
 
-// const Stack = createStackNavigator();
-// const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
+
+const TabComponent = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeBackgroundColor: 'skyblue',
+        activeTintColor: 'blue',
+        inactiveTintColor: 'white',
+        style: {
+          backgroundColor: '#c6cbef',
+        },
+        labelPosition: 'below-icon',
+      }}
+      screenOptions={({route}) => ({
+        tabBarLabel: route.name,
+        tabBarIcon: ({focused}) => TabBarIcon(focused, route.name),
+      })}>
+      <Tab.Screen name="Home" component={TabHomeScreen} />
+      <Tab.Screen name="User" component={TabUserScreen} />
+      <Tab.Screen name="Message" component={TabMessageScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const TabBarIcon = (focused, name) => {
   // let iconImagePath;
@@ -53,6 +81,40 @@ const TabBarIcon = (focused, name) => {
   );
 };
 
+const DrawerComponent = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerType="slide"
+      drawerPosition="right"
+      // eslint-disable-next-line react-native/no-inline-styles
+      drawerStyle={{
+        backgroundColor: '#c6cbef',
+        width: 200,
+      }}
+      drawerContentOptions={{
+        activeTintColor: 'red',
+        activeBackgroundColor: 'skyblue',
+      }}
+      drawerContent={props => <MyDrawer {...props} />}>
+      <Drawer.Screen name="Route" component={TabComponent} />
+    </Drawer.Navigator>
+  );
+};
+
+const HeaderRight = () => {
+  const navigation = useNavigation();
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flexDirection: 'row', paddingRight: 15}}>
+      <TouchableOpacity
+        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+        <Text>Open</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 class App extends Component {
   // CustomDrawerContent = props => {
   //   return (
@@ -70,26 +132,36 @@ class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName="Home"
-          tabBarOptions={{
-            activeBackgroundColor: 'skyblue',
-            activeTintColor: 'blue',
-            inactiveTintColor: 'white',
-            style: {
-              backgroundColor: '#c6cbef',
-            },
-            labelPosition: 'below-icon',
-          }}
-          screenOptions={({route}) => ({
-            tabBarLabel: route.name,
-            tabBarIcon: ({focused}) => TabBarIcon(focused, route.name),
-          })}>
-          <Tab.Screen name="Home" component={TabHomeScreen} />
-          <Tab.Screen name="User" component={TabUserScreen} />
-          <Tab.Screen name="Message" component={TabMessageScreen} />
-        </Tab.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={DrawerComponent}
+            options={{headerRight: ({}) => <HeaderRight />}}
+          />
+          <Stack.Screen name="Home_Stack" component={HomeScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
+      // <NavigationContainer>
+      //   <Tab.Navigator
+      //     initialRouteName="Home"
+      //     tabBarOptions={{
+      //       activeBackgroundColor: 'skyblue',
+      //       activeTintColor: 'blue',
+      //       inactiveTintColor: 'white',
+      //       style: {
+      //         backgroundColor: '#c6cbef',
+      //       },
+      //       labelPosition: 'below-icon',
+      //     }}
+      //     screenOptions={({route}) => ({
+      //       tabBarLabel: route.name,
+      //       tabBarIcon: ({focused}) => TabBarIcon(focused, route.name),
+      //     })}>
+      //     <Tab.Screen name="Home" component={TabHomeScreen} />
+      //     <Tab.Screen name="User" component={TabUserScreen} />
+      //     <Tab.Screen name="Message" component={TabMessageScreen} />
+      //   </Tab.Navigator>
+      // </NavigationContainer>
       // <NavigationContainer>
       //   <Drawer.Navigator
       //     initialRouteName="Home"
--- react_navigation_test/src/home_tab.js
+++ react_navigation_test/src/home_tab.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 export class TabHomeScreen extends Component {
   render() {
@@ -7,6 +7,10 @@ export class TabHomeScreen extends Component {
       // eslint-disable-next-line react-native/no-inline-styles
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Home</Text>
+        <Button
+          title="Go To Home"
+          onPress={() => this.props.navigation.navigate('Home_Stack')}
+        />
       </View>
     );
   }
